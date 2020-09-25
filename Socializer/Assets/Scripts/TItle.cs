01331e4e9f411140a3f1025d11af0aff18a3385d
m48483 <71882579+m48483@users.noreply.github.com>
2020-09-26 00:40:07 +0900

    [#1] 게임시작 함수 수정 및 게임종료 추가
    제 거랑 조금 달라서 좀 수정했어요! 원래 구현해두신 건 혹시 몰라서 주석 처리 해두었습니다.

--- Socializer/Assets/Scripts/TItle.cs
+++ Socializer/Assets/Scripts/TItle.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class TItle : MonoBehaviour
 {
-    public string s1="Gamestart";
+    //public string s1="Gamestart";
     void Start()
     {
         
@@ -14,9 +14,19 @@ public class TItle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(s1);
-        }
+        }*/
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gamestart");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
